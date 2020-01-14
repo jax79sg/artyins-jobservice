8773c65f06f172920869c068389674d9ecee3aa6
jax79sg <jax79sg@yahoo.com.sg>
2020-01-14 15:45:07 +0800

    update

--- runapp.sh
+++ runapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-gunicorn --bind 0.0.0.0:9898 --workers 1 wsgi:app
+gunicorn --bind 0.0.0.0:9891 --workers 10 wsgi:app
--- testsendreq.py
+++ testsendreq.py
@@ -1,20 +1,17 @@
 import requests 
+from config import JobConfig
 
-TEST_TYPE = 'flowers' #Options flowers sentiments
   
 # api-endpoint
-URL = None
-DATA = None
-if TEST_TYPE=='flowers':
-   URL = "http://localhost:9898/predict_flowers"
-   DATA = [{"petal_width":1.0, 'petal_length':2.0,'sepal_width':3.1,'sepal_length':4.3,}]
-elif (TEST_TYPE=='sentiments'): 
-   URL = "http://localhost:9898/predict_sentiments"
-   DATA = {'sentences':['Physical pain is like a norm to me nowadays','Its been a painful year','I still try hard to be relavant']}
-  
-# sending get request and saving the response as response object 
-r = requests.post(url = URL, json  = DATA) 
-print(r) 
-# extracting results in json format 
+config = JobConfig()
+
+URL = "http://localhost:9891/test"
+r = requests.post(url = URL, data = "")
+data=r.json()
+print("Data received: ", data["message"])
+
+URL = "http://localhost:9891/create_job"
+r = requests.post(url = URL, json = )
 data = r.json()
-print("Data sent:\n{}\n\nData received:\n{}".format(DATA,data))
+print("Data received: ", data["results"])
+
