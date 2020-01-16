4b0489adaf51645aa0a1b54c7aeb6ffa324d1879
jax79sg <jax79sg@yahoo.com.sg>
2020-01-16 15:49:42 +0800

    update

--- testsendreq.py
+++ testsendreq.py
@@ -5,7 +5,7 @@ from config import JobConfig
 # api-endpoint
 config = JobConfig()
 
-URL = "http://localhost:9891/test"
+URL = "http://127.0.0.1:9891/test"
 r = requests.get(url = URL, data = "")
 print("Data received: ", r)
 
